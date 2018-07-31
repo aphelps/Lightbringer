b648fd3f20dd89337c1b08572d0389d79540f05d
Adam Phelps <amp@opendns.com>
2018-07-31 01:14:59 -0500

    Committed READMEs and cylon test code from 2017

--- README.md
+++ README.md
@@ -2,8 +2,23 @@ Lightbringer boards
 -------------------
 
 * Start with setup instructions in https://github.com/aphelps/HMTL/wiki/First-steps
-* Depending on the board, these are the configuration files to use:
-  - Arduino nano: HMTL/python/configs/HMTLBoard_v4.json
+
+* Depending on the board, these are the configuration files to use (configs directory):
+  - Arduino nano: arduino_nano_hmtl_v1r5.json
   - Arduino mini:
   - Custom ATMEGA328:
   - Custom ATMEGA1284:
+
+* ...
+
+amp@AMP-MBP-home:~/Dropbox/Arduino/HMTL/platformio/HMTLPythonConfig [master]$ cd ../HMTLPythonConfig/
+amp@AMP-MBP-home:~/Dropbox/Arduino/HMTL/platformio/HMTLPythonConfig [master]$ platformio run -t upload -e nano
+amp@AMP-MBP-home:~/Dropbox/Arduino/Lightbringer/configs [master]$ HMTLConfig -v -f arduino_nano_hmtl_v1r5.json  -w -a 2 -i 2
+
+*** 328 ***
+
+amp@AMP-MBP-home:~/Dropbox/Arduino/HMTL/platformio/HMTLPythonConfig [master]$ cd ../HMTLPythonConfig/
+amp@AMP-MBP-home:~/Dropbox/Arduino/HMTL/platformio/HMTLPythonConfig [master]$ platformio run -t upload -e uno
+amp@AMP-MBP-home:~/Dropbox/Arduino/Lightbringer/configs [master]$ HMTLConfig -v -f /Users/amp/Dropbox/Arduino/Lightbringer/configs/HMTLv10_328_5x69.json -a XXX -i XXX -w
+
+amp@AMP-MBP-home:~/Dropbox/Arduino/HMTL/platformio/HMTL_Module [master]$ platformio run -e lightbringer_328 -t upload
---
+++ src/cylon/cylon.ino
@@ -0,0 +1,70 @@
+#include "FastLED.h"
+
+#ifndef NUM_LEDS
+// How many leds in your strip?
+  #define NUM_LEDS 69*5
+#endif
+
+#ifndef BRIGHTNESS
+  #define BRIGHTNESS 128
+#endif
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  boolean initialized = false;
+
+  Serial.begin(9600);
+  Serial.println("Lightbringer cylon");
+#ifdef ATMEGA328_SOFT
+  initialized = true;
+  LEDS.addLeds<APA102, 11, 13, RGB, DATA_RATE_MHZ(1)>(leds, NUM_LEDS);
+#endif
+#ifdef ATMEGA1284_SOFT
+  initialized = true;
+  LEDS.addLeds<APA102, 5, 7, RGB, DATA_RATE_MHZ(1)>(leds, NUM_LEDS);
+#endif
+
+  if (!initialized) {
+    LEDS.addLeds<APA102, 11, 13, RGB, DATA_RATE_MHZ(1)>(leds, NUM_LEDS);
+  }
+
+  LEDS.setBrightness(BRIGHTNESS);
+
+  Serial.print("NUMLEDS:");
+  Serial.println(NUM_LEDS);
+}
+
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
+void loop() {
+  static uint8_t hue = 0;
+  Serial.print("x");
+  // First slide the led in one direction
+  for(int i = 0; i < NUM_LEDS; i++) {
+    // Set the i'th led to red
+    leds[i] = CHSV(hue++, 255, 255);
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }
+  Serial.print("x");
+
+  // Now go in the other direction.
+  for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+    // Set the i'th led to red
+    leds[i] = CHSV(hue++, 255, 255);
+    // Show the leds
+    FastLED.show();
+    // now that we've shown the leds, reset the i'th led to black
+    // leds[i] = CRGB::Black;
+    fadeall();
+    // Wait a little bit before we loop around and do it again
+    delay(10);
+  }
+}
